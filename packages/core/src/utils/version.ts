cea14ed1ff501dcf453b6623a566de1d6596f78e
Tiago Santos <tvcsantos@gmail.com>
2026-02-16 10:40:26 +0000

    feat: fix versioning and add contributing guides

--- packages/core/src/utils/version.ts
+++ packages/core/src/utils/version.ts
@@ -1,4 +1,4 @@
 // This file is auto-generated. Do not edit manually.
 // Run 'npm run generate-version' to update this file.
-export const VERSION = "0.6.11";
+export const VERSION = "0.6.13";
 export const PACKAGE_NAME = "@versu/core";
--- packages/plugin-gradle/src/utils/version.ts
+++ packages/plugin-gradle/src/utils/version.ts
@@ -1,5 +1,5 @@
 // This file is auto-generated. Do not edit manually.
 // Run 'npm run generate-version' to update this file.
-export const VERSION = "0.6.11";
+export const VERSION = "0.6.13";
 export const PACKAGE_NAME = "@versu/plugin-gradle";
 export const AUTHORS = "tvcsantos";
